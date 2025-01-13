68555984e9dc82e5afed2c65e76ab55e3abd0aed
PackeLo <123914280+PackeLo@users.noreply.github.com>
2025-01-13 11:24:20 +0000

    ドメイン所有者確認情報追加

--- src/app.vue
+++ src/app.vue
@@ -65,6 +65,7 @@ const helpTrigger = () => {
     <meta name="description" content="ページ数を入力するだけで簡単に、中綴じ製本の面付けをシミュレーションします。製本作業の目安にご利用ください。">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1620906024044943"
      crossorigin="anonymous"></script>
+    <meta name="google-site-verification" content="6r4SjTQqEqm9lt2fJwNnyQ_t51dTiTRtQ-TjpZK6JXA" />
   </Head>
 
   <!-- 画面 -->
